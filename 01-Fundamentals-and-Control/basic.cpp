fa7a1dbb235cddf80596e49ce13ca832c9732327
Farhan Rabby <farhanrabby59@gmail.com>
2026-03-25 20:26:12 +0600

    identifiers, keyword, variables

---
+++ 01-Fundamentals-and-Control/basic.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Integer variable
+    int age = 25;
+    
+    // Floating-point variable
+    double salary = 45000.50;
+    
+    // Character variable
+    char grade = 'A';
+    
+    // String variable
+    string name = "John Doe";
+    
+    // Boolean variable
+    bool isStudent = true;
+    
+    // Display variables
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Salary: " << salary << endl;
+    cout << "Is Student: " << isStudent << endl;
+    
+    return 0;
+}
