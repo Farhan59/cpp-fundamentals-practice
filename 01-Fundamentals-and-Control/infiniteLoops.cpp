86d021ed0155445c90b0fd0abd3823155e05c5fe
Farhan Rabby <farhanrabby59@gmail.com>
2026-03-27 19:02:08 +0600

    loop

---
+++ 01-Fundamentals-and-Control/infiniteLoops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a=1;
+
+    // Infinite loop with for
+    for (;;) {
+        cout <<a<< ". Infinite for loop" << endl;a++;
+        
+    }
+
+    
+
+    return 0;
+}
